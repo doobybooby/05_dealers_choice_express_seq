0207669f3c7a8d33c12bfe72a0332f4fdbf5e413
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-05-26 10:54:29 -0400

    clean up

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', (req,res,next)=> {
       <link rel='stylesheet' href='/public/styles.css' />
     </head>
     <body>
-      <h1>
-        hello world
-      </h1>
+      
     </body>
   `)
 })
