31d4cbe5daa21b39ea1fbd7001a69d78e4006664
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-05-26 11:36:29 -0400

    before sequelize  association

--- database/db.js
+++ database/db.js
@@ -17,16 +17,33 @@ const Player = conn.define('player', {
   }
 })
 
+const Room = conn.define('room', {
+  roomName: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull:false
+  }
+})
+
+
 
 const syncAndSeed = async()=> {
   try{
     await conn.sync({ force:true })
+    
     const lory = await Promise.all([Player.create({username:'lory'})])
     const [tom, bob, joe, moe] = await Promise.all(
       ['tom', 'bob', 'joe', 'moe'].map( n => {
         return Player.create({ username:n })
       })
     )
+    
+    const [room1, room2, room3, room4] = await Promise.all(
+      ['room1', 'room2', 'room3', 'room4'].map( n => {
+        return Room.create({ roomName:n })
+      })
+    )
+
   }
   catch(ex){
     console.log(ex)
@@ -34,5 +51,9 @@ const syncAndSeed = async()=> {
 }
 
 module.exports = {
-  syncAndSeed
+  syncAndSeed,
+  models:{
+    Player,
+    Room
+  }
 }
--- server.js
+++ server.js
@@ -1,19 +1,176 @@
-const { syncAndSeed } = require('./database/db')
+const { syncAndSeed, models: { Player, Room } } = require('./database/db')
 const express = require('express')
 const app = express()
+app.use(express.urlencoded())
 app.use('/public', express.static('public'))
 
-app.get('/', (req,res,next)=> {
+app.get('/', async(req,res,next)=> {
+  const [ players, rooms ] = await Promise.all([
+    Player.findAll(),
+    Room.findAll()
+  ])
+
   res.send(`
     <head>
       <link rel='stylesheet' href='/public/styles.css' />
     </head>
     <body>
-      
+        <nav>
+          <a href='/' class='selected'> HOME </a>
+          <a href='/players'> PLAYERS </a>
+          <a href='/rooms'> ROOMS </a>
+        </nav>
+        <div class='mainDiv'>
+          <h3>This is home </h3>
+          <div>
+            <h5>ACTIVE PLAYERS: ${players.length}<h5>
+            <h5>AVAILABLE ROOMS: ${rooms.length}<h5>
+          </div>
+        </div>
     </body>
   `)
 })
 
+app.get('/players', async(req,res,next)=> {
+  const [ players, rooms ] = await Promise.all([
+    Player.findAll(),
+    Room.findAll()
+  ])
+
+  res.send(`
+    <head>
+      <link rel='stylesheet' href='/public/styles.css' />
+    </head>
+    <body>
+        <nav>
+          <a href='/' > HOME </a>
+          <a href='/players' class='selected'> PLAYERS </a>
+          <a href='/rooms' > ROOMS </a>
+        </nav>
+        <div class='mainDiv'>
+          <h3>Players List </h3>
+          <div>
+            <ul>${players.map(player => {
+              return `
+                <li>
+                  <a href='/players/${player.id}'>${player.username}</a>
+                </li>
+              `
+            }).join('')}<ul>
+          </div>
+        </div>
+    </body>
+  `)
+})
+
+
+app.get('/players/:id', async(req,res,next)=> {
+  const id = req.params.id
+  const [ player, rooms ] = await Promise.all([
+    Player.findAll({
+      where: {
+        id:id
+      }
+    }),
+    Room.findAll()
+  ])
+
+  res.send(`
+    <head>
+      <link rel='stylesheet' href='/public/styles.css' />
+    </head>
+    <body>
+        <nav>
+          <a href='/' > HOME </a>
+          <a href='/players' class='selected'> PLAYERS </a>
+          <a href='/rooms' > ROOMS </a>
+        </nav>
+        <div class='mainDiv'>
+          <div>
+            <h3>PLAYER ID: ${req.params.id} </h3>
+            <ul>${player.map(p => {
+              return `
+                    <li>
+                      ${JSON.stringify(p)}
+                    </li>
+                  `
+                }).join('')}
+            <ul>
+          </div>
+        </div>
+    </body>
+  `)
+})
+
+
+app.get('/rooms', async(req,res,next)=> {
+  const [rooms] = await Promise.all(
+    [Room.findAll()]
+  )
+
+  res.send(`
+  <head>
+      <link rel='stylesheet' href='/public/styles.css' />
+    </head>
+    <body>
+      <nav>
+        <a href='/' > HOME </a>
+        <a href='/players' > PLAYERS </a>
+        <a href='/rooms' class='selected'> ROOMS </a>
+      </nav>
+      <div class='mainDiv'>
+        <ul>${rooms.map(room=> {
+          return `
+            <li><a href='/rooms/${room.id}'>${room.roomName}</a></li>
+          `
+        }).join('')}<ul>
+      </div>
+    </body>
+  `)
+})
+
+
+app.get('/rooms/:id', async(req,res,next)=> {
+  const id = req.params.id
+  const [ player, room ] = await Promise.all([
+    Player.findAll({
+      where: {
+        id:id
+      }
+    }),
+    Room.findAll({
+      where:{
+        id:id
+      }
+    })
+  ])
+
+  res.send(`
+    <head>
+      <link rel='stylesheet' href='/public/styles.css' />
+    </head>
+    <body>
+        <nav>
+          <a href='/' > HOME </a>
+          <a href='/players' class='selected'> PLAYERS </a>
+          <a href='/rooms' > ROOMS </a>
+        </nav>
+        <div class='mainDiv'>
+          <div>
+            <h3>ROOM ID: ${req.params.id} </h3>
+            <ul>${room.map(p => {
+              return `
+                    <li>
+                      ${JSON.stringify(p)}
+                    </li>
+                  `
+                }).join('')}
+            <ul>
+          </div>
+        </div>
+    </body>
+  `)
+})
 
 
 
